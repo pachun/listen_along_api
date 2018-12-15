24bf0477de5a6c1072a9c0aa3f5b5bdee0b094c6
Nick Pachulski <nick@pachulski.me>
2018-12-15 01:29:26 -0500

    Default root route to spotify users administration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root to: "admin/spotify_users#index"
 
   resources :broadcasters, only: [:index]
   resources :listen_along, only: [:index]
