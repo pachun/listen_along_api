a387a7b375783c6cc62b5a4737f6d59a35b56bad
Nick Pachulski <nick@pachulski.me>
2018-12-16 01:40:38 -0500

    Remove unneeded fields from spotify users AA index

--- app/admin/spotify_users.rb
+++ app/admin/spotify_users.rb
@@ -9,4 +9,14 @@ ActiveAdmin.register SpotifyUser do
     :millisecond_progress_into_song,
     :is_listening,
     :broadcaster
+
+  index do
+    actions
+    id_column
+    column :username
+    column :broadcaster
+    column :song_name
+    column :millisecond_progress_into_song
+    column :is_listening
+  end
 end
