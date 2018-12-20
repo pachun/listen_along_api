6637c9c9ec059658035276f763c54a886b5114c4
Nick Pachulski <nick@pachulski.me>
2018-12-20 16:48:57 -0500

    Fix bug causing lost db pool connections
    After some time running (usually about 24 hours), the Rufus-scheduler task begins to fail, indicating:
    
    > ActiveRecord::ConnectionTimeoutError (could not obtain a connection from the pool within 5.000 seconds (waited 5.000 seconds); all pooled connections were in use):
    
    Someone’s response in a Rufus-scheduler issue thread here suggests this will solve the problem:
    
    https://github.com/jmettraux/rufus-scheduler/issues/14#issuecomment-1026095

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,5 +3,14 @@ require "rufus-scheduler"
 scheduler = Rufus::Scheduler.singleton
 
 scheduler.every "5s" do
-  UpdatePlaybackService.update
+  begin
+    unless ActiveRecord::Base.connected?
+      ActiveRecord::Base.connection.verify!(0)
+    end
+    UpdatePlaybackService.update
+  rescue => e
+    status e.inspect
+  ensure
+    ActiveRecord::Base.connection_pool.release_connection
+  end
 end
