48844314f127fc5b350df7d694acb8d5203fff3a
Nick Pachulski <nick@pachulski.me>
2019-09-21 16:37:29 -0400

    Update expired users avatars synchronously
    There are way too many users to update their avatars asynchronously at this point (115 currently).

--- app/commands/refresh_expired_avatars.rb
+++ app/commands/refresh_expired_avatars.rb
@@ -5,39 +5,31 @@ class RefreshExpiredAvatars
     new.refresh
   end
 
-  attr_accessor :threads, :updated_avatar_states
+  attr_accessor :updated_avatar_states
 
   def initialize
-    @threads = []
     @updated_avatar_states = []
   end
 
   def refresh
-    request_updated_expired_avatars_in_parallel
-    wait_for_requests_to_finish
+    request_updated_expired_avatars
     update_expired_avatars
   end
 
   private
 
-  def request_updated_expired_avatars_in_parallel
+  def request_updated_expired_avatars
     SpotifyUser.all.each do |spotify_user|
       request_updated_avatar_for(spotify_user)
     end
   end
 
   def request_updated_avatar_for(spotify_user)
-    threads << Thread.new do
-      if Faraday.get(spotify_user.avatar_url).status == FORBIDDEN
-        updated_avatar_states << SpotifyService
-          .new(spotify_user)
-          .updated_avatar_state
-      end
+    if Faraday.get(spotify_user.avatar_url).status == FORBIDDEN
+      updated_avatar_states << SpotifyService
+        .new(spotify_user)
+        .updated_avatar_state
     end
-  end
-
-  def wait_for_requests_to_finish
-    threads.each(&:join)
   end
 
   def update_expired_avatars
