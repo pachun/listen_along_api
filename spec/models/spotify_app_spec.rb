0b13fd7b40e49fe4b2f4d09cd5367b09eb64356f
Nick Pachulski <nick@pachulski.me>
2019-11-28 20:19:20 -0500

    Test SpotifyApp.with_most_spotify_users scope

--- spec/models/spotify_app_spec.rb
+++ spec/models/spotify_app_spec.rb
@@ -1,6 +1,22 @@
 require "rails_helper"
 
 describe SpotifyApp do
+  describe ":with_most_spotify_users scope" do
+    it "returns the spotify app with the most spotify users" do
+      app_1 = create :spotify_app
+      create :spotify_user, spotify_app: app_1
+
+      app_2 = create :spotify_app
+
+      expect(SpotifyApp.with_most_spotify_users).to eq(app_1)
+
+      create :spotify_user, spotify_app: app_2
+      create :spotify_user, spotify_app: app_2
+
+      expect(SpotifyApp.with_most_spotify_users).to eq(app_2)
+    end
+  end
+
   describe "self.concurrently_updatable_user_batches(batch_size:)" do
     it "returns batches of concurrently updatable spotify users" do
       app_1 = create :spotify_app
@@ -9,7 +25,7 @@ describe SpotifyApp do
       app_2 = create :spotify_app
       app_2_users = (0..1).map { create :spotify_user, spotify_app: app_2 }
 
-      app_3 = create :spotify_app
+      create :spotify_app
 
       batches = SpotifyApp
         .concurrently_updatable_spotify_user_batches(batch_size: 2)
